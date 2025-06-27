31c2d885e3171cefc8561654fc5e096163f1a78a
GuyMangan <guy.mangan@icloud.com>
2025-06-27 14:47:18 -0400

    Update CodeRaman.py
    Plot
    Find Peaks
    Find Local Minimum between Peaks

--- CodeRaman.py
+++ CodeRaman.py
@@ -7,34 +7,12 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.signal import deconvolve
 
-#Functions//Fonctions
- # def CheckForText(data):
- #  """
- #  Checks file for text at any point during it
- #  Args:
- #  data : np.ndarray
- #        The datafile being checked
-
- #  Returns : 
- #  result : int
- #          1 if there is an error
- #          0 if there is no error
- #  """
-  
-
-#Load Files//Charger des fichiers
+#Load Files//Charger des fichiers   
 datafile = '<PATH>'
-data = np.genfromtxt(datafile,delimiter=' ',unpack=True)
-
-#Replace Commas With Points//Remplacer les virgules par des points
-data = np.char.replace(data,",",".")
+data = np.genfromtxt(datafile,delimiter='',unpack=True, skip_header = 0, dtype=float)
 
 #Split to x and y components//Diviser en composantes x et y
 x,y = data
-
-#Checks//Vérifications
-# if CheckForText(data) == 1:
-#   print("Ensure File has no headers or other text//Assurez-vous que le fichier ne contient pas d'en-têtes ou d'autre texte")
 
 #Plotting//Traçage
 fig = plt.figure()
@@ -46,15 +24,26 @@ ax.set_xlabel('Raman Shift (cm$^{-1}$)')
 ax.set_ylim(0)
 ax.set_ylabel('Intensity (counts)')
 ax.set_title('Un-deconvolved Raman Spectrum')
+
+#Find Peaks//Trouver des sommets
+peaks, properties = find_peaks(y,
+                               height=y.mean(),
+                               prominence=6,
+                               distance=20)
+
+#Find Local Minimum//Trouver le minimum local
+valley_y = min(y[peaks[0]:peaks[1]])
+valley_x = (x[np.where(y == valley_y)[0]])[0]
+valley_x,valley_y
+
+ax.axvline(x[peaks[0]],color = 'r',label = f'D Band:{x[peaks[0]]}')
+ax.axvline(x[peaks[1]],color = 'g',label = f'G Band: {x[peaks[1]]}')
+ax.axvline(valley_x,color = 'y',label = f'Local Minimum: {valley_x}')
+#ax.axvline(x[peaks[2]],color = 'pink',label = f'D Band:{x[peaks[2]]}')
+ax.legend()
 plt.show()
 
-#Finding Peaks//Trouver des sommets
-find_peaks(data,
-           height=,
-           prominence=)
-
-#
-
-
-
-
+#Print Results//Imprimer les résultats
+print(f'D-Band: {x[peaks[0]]}')
+print(f'G Band: {x[peaks[1]]}')
+print(f'Local Minimum: {valley_x}')
